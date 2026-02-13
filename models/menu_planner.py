c7df188e14d8c9ddc84499bf028251f0c69aa225
Erich Imfeld <eimfeld@gmx.net>
2026-02-13 17:40:06 +0100

    Adjust required fields in model and dialog
    effort level, category and seasons

--- models/menu_planner.py
+++ models/menu_planner.py
@@ -26,12 +26,12 @@ class Menu(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     name: str = Field(unique=True)
     description: Optional[str] = None
-    category_id: Optional[int] = Field(default=None, foreign_key="menucategory.id")
-    effort_level_id: Optional[int] = Field(default=None, foreign_key="effortlevel.id")
+    category_id: int = Field(foreign_key="menucategory.id")
+    effort_level_id: int = Field(foreign_key="effortlevel.id")
     to_take_away: bool = False
     protein: float
 
 
 class MenuSeasonLink(SQLModel, table=True):
-    menu_id: Optional[int] = Field(default=None, foreign_key="menu.id", primary_key=True)
-    season_id: Optional[int] = Field(default=None, foreign_key="season.id", primary_key=True)
+    menu_id: int = Field(foreign_key="menu.id", primary_key=True)
+    season_id: int = Field(foreign_key="season.id", primary_key=True)
